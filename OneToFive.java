59f679d9803ce966eae47eba36d1ce68c8c4ef4f
Kyjorn <hendonmshane@gmail.com>
2025-05-04 15:39:15 -0600

    refactor OneToFive class to calculate and print squares of numbers 1 to 5

--- OneToFive.java
+++ OneToFive.java
@@ -2,9 +2,13 @@ public  class OneToFive {
 
     public static void main(String[] args) {
         
+        int initalNum;
+        int squaredNum;
+
         for (int i = 1; i <= 5; i++) {
-            
-           System.out.println(i);
+            initalNum = i;
+            squaredNum = i * i;
+            System.out.println(initalNum + " * " + i + " = " + squaredNum);
             
         }
     }
